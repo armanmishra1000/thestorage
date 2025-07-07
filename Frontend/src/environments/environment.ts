f498b0d55036d51ad9d4c7b153205a213d53ee7e
mitaliborad <emmagarcia91992@gmail.com>
2025-07-07 09:48:17 +0530

    delete frontend

--- Frontend/src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-// Frontend/src/environments/environment.prod.ts
-export const environment = {
-  production: true,
-  apiUrl: 'https://mitali-backend-f8xd.onrender.com/api/v1', // We will replace this later
-  wsUrl: 'wss://mitali-backend-f8xd.onrender.com/ws_api/ws' // We will replace this later
-};
